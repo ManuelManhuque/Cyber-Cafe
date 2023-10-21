0405a8dda06b45849ba55ada516f37f720838d65
ManuelManhuque <144432918+ManuelManhuque@users.noreply.github.com>
2023-10-22 01:30:15 +0200

    act

--- JS/comportamento.js
+++ JS/comportamento.js
@@ -31,11 +31,8 @@ window.onscroll = () => {
 
 // aqui comeca o comportamento do login
 const container = document.getElementById('container');
-
 const registerBtn = document.getElementById('register');
-
 const loginBtn = document.getElementById('login');
-
 
 registerBtn.addEventListener('click', () => {
     container.classList.add("active");
@@ -45,3 +42,37 @@ loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
 });
 // aqui termina o comportamento do login
+
+
+// dropdown menu comeca aqui
+
+const dropdowns = document.querySelectorAll('.dropdown');
+
+dropdowns.forEach(dropdown => {
+    const select = dropdown.querySelector('.select');
+    const caret = dropdown.querySelector('.caret');
+    const menu = dropdown.querySelector('.menu');
+    const options = dropdown.querySelectorAll('.menu li');
+    const selected = dropdown.querySelector('.selected');
+
+    select.addEventListener('click', () => {
+        select.classList.toggle('select-clicked');
+        caret.classList.toggle('select-rotate');
+        menu.classList.toggle('menu-open');
+    });
+
+    options.forEach(option => {
+        select.innerText = option.innerText;
+        select.classList.remove('select-clicked');
+        caret.classList.remove('caret-rotate');
+        menu.classList.remove('menu-open');
+
+        options.forEach(option =>{
+            option.classList.remove('active');
+        });
+        option.classList.add('active')
+    
+
+    });
+});
+// dropdown menu acaba aqui
